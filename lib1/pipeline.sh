0f26b80a5d4f9698d71b292e1abf96d5c301ce1a
kcherenkovv <152647935+kcherenkovv@users.noreply.github.com>
2024-05-13 19:31:13 +0300

    Update pipeline.sh

--- lib1/pipeline.sh
+++ lib1/pipeline.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # Запуск Python-скриптов по порядку
-python3 data_creation.py
-python3 model_preprocessing.py
-python3 model_preparation.py
-python3 model_testing.py
+python3 scripts/data_creation.py
+python3 scripts/model_preprocessing.py
+python3 scripts/model_preparation.py
+python3 scripts/model_testing.py
