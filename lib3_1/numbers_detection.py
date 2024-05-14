67a3e2dcb4671fa081cddf18da147323ec5ba944
kcherenkovv <kcherenkov9888@mail.ru>
2024-05-14 22:31:45 +0300

    Add new files

---
+++ lib3_1/numbers_detection.py
@@ -0,0 +1,30 @@
+import yolov5
+from PIL import Image
+import streamlit as st
+
+
+@st.cache_data
+def load_model():
+    model = yolov5.load('keremberke/yolov5m-license-plate')
+    model.conf = 0.25  # NMS confidence threshold
+    model.iou = 0.45  # NMS IoU threshold
+    model.agnostic = False  # NMS class-agnostic
+    model.multi_label = False  # NMS multiple labels per box
+    model.max_det = 1000  # maximum number of detections per image
+    return model
+
+
+def process_image(model, img):
+    results = model(img, size=640)
+    results = model(img, augment=True)
+    # predictions = results.pred[0]
+    # boxes = predictions[:, :4]  # x1, y1, x2, y2
+    # scores = predictions[:, 4]
+    # categories = predictions[:, 5]
+    # numpy_image = results.render()[0]
+    # numpy_image = results.render()[1]
+    numpy_images = results.crop(save=False)
+    output_imgs = list()
+    for numpy_img in numpy_images:
+        output_imgs.append(Image.fromarray(numpy_img['im']))
+    return output_imgs
---
+++ lib3_1/web_view.py
@@ -0,0 +1,35 @@
+import io
+import streamlit as st
+from PIL import Image
+
+
+def load_image():
+    uploaded_file = st.file_uploader(
+        label='Выберите изображение, на котором изображен автомобиль с номером')
+    if uploaded_file is not None:
+        image_data = uploaded_file.getvalue()
+        st.image(image_data)
+        return Image.open(io.BytesIO(image_data))
+    else:
+        return None
+
+
+def init():
+    # Выводим заголовок страницы средствами Streamlit
+    st.title('Поиск автомобильных номеров на изображении')
+
+
+def show_result(number_image, number_text):
+    st.image(number_image)
+    if number_text is None or len(number_text) == 0:
+        st.subheader('Не удалось распознать номер на этом фрагменте')
+    else:
+        st.subheader(number_text)
+
+
+def show_result_small(number_image):
+    st.image(number_image)
+
+
+def show_result_title():
+    st.title("Результаты поиска:")
