03b0f14095faf9691ba1dcb8791dc4759ebf60b5
kcherenkovv <kcherenkov9888@mail.ru>
2024-05-22 17:10:54 +0300

    Add datasets

--- lib4/data_get.py
+++ lib4/data_get.py
@@ -4,7 +4,7 @@ from catboost.datasets import titanic
 titanic_train, titanic_test = titanic()
 
 
-titanic_train = titanic_train['Pclass','Sex','Age']
+titanic_train = titanic_train[['Pclass','Sex','Age']]
 
 #Сохраняем датасет в формате csv
 titanic_train.to_csv('titanic_train.csv', index=False)
